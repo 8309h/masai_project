3670a2f89835af1fb8f13c28ef410c19a405178b
8309h <harshalwagh201718@gmail.com>
2022-09-13 22:12:30 +0530

    palindrome number check

---
+++ login.js
@@ -0,0 +1,11 @@
+let temp=num;
+     final=0;
+    while(num>0){
+        rem = num%10;
+	   num= Math.floor(num/10);
+	    
+    }if(final==temp){
+        console.log("Yes");
+    }else{
+        
+    }
